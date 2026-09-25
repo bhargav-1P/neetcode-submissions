6c997e674bfd027fd932f3c7e44c6328be120ff5
PULA BHARGAV RAM <bhargavrampula@gmail.com>
2026-09-25 09:41:45 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length!==t.length){
+            return false
+        }
+        const counts={}
+        const countt={}
+        for(let i=0; i<s.length;i++){
+            counts[s[i]]=(counts[s[i]] || 0) +1;
+            countt[t[i]]=(countt[t[i]] || 0) +1;
+        }
+        for(const  c  in counts){
+            if (counts[c]!== countt[c]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
